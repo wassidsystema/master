dff65b96acbf675e17223ec6cf4ecf3dcf63e84c
wlclimaco85 <wlclimaco@hotmail.com>
2019-02-12 00:56:13 -0200

    dsadasdsa

--- src/main/java/com/nouhoun/springboot/jwt/integration/data/DicionarioHelper.java
+++ src/main/java/com/nouhoun/springboot/jwt/integration/data/DicionarioHelper.java
@@ -3,7 +3,7 @@ package com.nouhoun.springboot.jwt.integration.data;
 public abstract class DicionarioHelper{
 
 	public static String insert() {
-		String sql = "INSERT INTO MENU (menu_id, label, nivel, nome, status) VALUES ('1', 'Pagina Inicial', '0', 'Pagina Inicial', '0')";
+		String sql = "INSERT INTO MENU (menu_id,label, nivel, nome, status) VALUES (1000,'Pagina Inicial', '0', 'Pagina Inicial', '0')";
 
 		return sql;
 	}
@@ -19,7 +19,7 @@ public abstract class DicionarioHelper{
 	}
 	
 	public static String fetchPage() {
-		String sql = "SELECT * FROM MENU";
+		String sql = "SELECT * FROM MENU where menu_id = 1";
 		return sql;
 	}
 }
